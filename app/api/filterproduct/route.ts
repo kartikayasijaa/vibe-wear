780ab0c9043bc26bc3614f30ee245fb8f393266a
Kartikay Asija <kartikay7057@gmail.com>
2023-10-19 01:59:42 +0530

    bugg

--- app/api/filterproduct/route.ts
+++ app/api/filterproduct/route.ts
@@ -1,35 +1,30 @@
 import prisma from "@/app/prismadb";
 import { NextResponse } from "next/server";
-export const dynamic = 'auto'
+export const dynamic = "auto";
 // 'auto' | 'force-dynamic' | 'error' | 'force-static'
 
 export async function GET(request: Request) {
-  try {
-    const searchParams = new URLSearchParams(request.url.split('?')[1]);
-    const categories = searchParams.getAll("categories[]");
-    const colors = searchParams.getAll("colors[]");
-    const sizes = searchParams.getAll("size[]");
-    const minPrice = parseInt(searchParams.get("price[min]") || "0");
-    const maxPrice = parseInt(searchParams.get("price[max]") || "100000");
+  const searchParams = new URLSearchParams(request.url.split("?")[1]);
+  const categories = searchParams.getAll("categories[]");
+  const colors = searchParams.getAll("colors[]");
+  const sizes = searchParams.getAll("size[]");
+  const minPrice = parseInt(searchParams.get("price[min]") || "0");
+  const maxPrice = parseInt(searchParams.get("price[max]") || "100000");
 
-    const products = await prisma.product.findMany({
-      where: {
-        OR: [
-          ...categories.map((category) => ({ style: { contains: category } })),
-          ...sizes.map((size) => ({ size: { contains: size } })),
-          ...colors.map((color) => ({ color: { contains: color } })),
-          { price: { gte: minPrice, lte: maxPrice } },
-        ],
-      },
-    });
+  const products = await prisma.product.findMany({
+    where: {
+      AND: [
+        ...categories.map((category) => ({ style: { contains: category } })),
+        ...sizes.map((size) => ({ size: { contains: size } })),
+        ...colors.map((color) => ({ color: { contains: color } })),
+        { price: { gte: minPrice, lte: maxPrice } },
+      ],
+    },
+  });
 
-    if(!products){
-      return NextResponse.json({err: "no products found"})
-    }
-
-    return NextResponse.json(products);
-  } catch (error) {
-    console.error("Error selecting product", error);
-    return NextResponse.error();
+  if (!products) {
+    return NextResponse.json({ err: "no products found" });
   }
+
+  return NextResponse.json(products);
 }
